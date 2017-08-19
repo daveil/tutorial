ad0ef7417b87a796b51f45daaff94456d1f782b4
Dave Alaras <arroyo.daveil@gmail.com>
2017-08-19 13:07:29 +0800

    Update homepage

--- index.php
+++ index.php
@@ -1,10 +1,33 @@
 <html>
 	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<!-- Latest compiled and minified CSS -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 	</head>
-	<body>
-		<h1>Hello World</h1>
+	<body class="container-fluid">
+		<div class="row">
+			<div class="col-md-6 col-xs-3">
+				<h1>Hello</h1>
+			</div>
+			<div class="col-md-6 col-xs-9">
+				<h1>World</h1>
+			</div>
+		</div>
+		<div class="row">
+			
+			<div class="col-md-12">
+				<form action="">
+					<div class="form-group">
+						<input type="email" placeholder="Email" class="form-control">
+					</div>
+					<div class="form-group">
+						<input type="number" placeholder="Number" class="form-control">
+					</div>
+				</form>
+
+			</div>
+		</div>
+		
 		<!-- Latest compiled and minified JavaScript -->
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 	</body>
